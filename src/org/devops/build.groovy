5b528f828ca9d855e4c60c3d82d171060966b704
fallenjlz <32124530+fallenjlz@users.noreply.github.com>
2022-05-31 23:59:48 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,11 +1,14 @@
 package org.devops
 
 def Build(buildType,buildShell){
-    def buildTools = ["gradle":"GRADLE"]
+    def buildTools = ["mvn":"mnv","ant":"ant","gradle":"GRADLE"]
     
     
     println("当前选择的构建类型为 ${buildType}")
-    buildHome= tool buildTools[buildType]
-    
-    sh "${buildHome}/bin/${buildType}  ${buildShell}"
+    if ("${buildType}" == "gradle"){
+        buildHome= tool buildTools[buildType]
+        sh "${buildHome}/bin/${buildType}  ${buildShell}"
+    }else {
+        sh "${buildType} -v"
+    }
  }
