1ac1346f5e3ebc34576d1b70f4fdf9410f915f39
llo <iioelse@gmail.com>
2022-06-01 21:34:25 +0800

    change build

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,14 +1,14 @@
 package org.devops
 
 def Build(buildType,buildShell){
-    def buildTools = ["maven":"mvn","ant":"ant","gradle":"GRADLE"]
+    def buildTools = ["mvn":"M2","ant":"ant","gradle":"GRADLE"]
     
     
     println("当前选择的构建类型为 ${buildType}")
-    if ("${buildType}" == "gradle"){
-        buildHome= tool buildTools[buildType]
-        sh "${buildHome}/bin/${buildType}  ${buildShell}"
-    }else {
+    if ("${buildType}" == "ant "){
         sh "${buildTools[buildType]} ${buildShell}"
+    }else {
+         buildHome= tool buildTools[buildType]
+         sh "${buildHome}/bin/${buildType}  ${buildShell}"
     }
  }
